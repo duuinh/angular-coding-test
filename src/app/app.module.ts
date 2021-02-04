c6860736e03c96c8af8966fcb0c239759fb9f6f9
Natnipa S <s.natnipa@gmail.com>
2021-02-04 19:00:21 +0700

    update app module: add components, httpClient, formModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { QuestionOneComponent } from './question-one/question-one.component';
+import { HomeComponent } from './home/home.component';
+import { QuestionTwoComponent } from './question-two/question-two.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionOneComponent,
+    HomeComponent,
+    QuestionTwoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
