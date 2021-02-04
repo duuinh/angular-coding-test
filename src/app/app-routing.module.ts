3d1cec9f260493b6b8180800bc8c4c872679b040
Natnipa S <s.natnipa@gmail.com>
2021-02-04 18:52:36 +0700

    add routes: home, question1, question2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { QuestionOneComponent } from './question-one/question-one.component';
+import { QuestionTwoComponent } from './question-two/question-two.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'question-1', component: QuestionOneComponent },
+  { path: 'question-2', component: QuestionTwoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
